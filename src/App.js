f44b61158006e3d17f5696dc7d432a5233e9c4fd
edoardodominikus <s108006222@m108.nthu.edu.tw>
2022-09-22 16:47:18 +0800

    added toastcontainer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import 'assets/scss/style.scss' 
 import { BrowserRouter as Router,Routes,Route,withRouter,Switch} from 'react-router-dom'
+import { ToastContainer } from "react-toastify";
 import LandingPage from "pages/LandingPage";
 import DetailsPage from "pages/DetailsPage";
 import Checkout from "pages/Checkout";
@@ -16,8 +17,10 @@ function App() {
           <Route exact path="/" component={LandingPage}></Route>
           <Route exact path="/properties/:id" component={withRouter(DetailsPage)}></Route>
           <Route exact path="/checkout" component={Checkout}></Route>
-
       </Router>
+      <ToastContainer>
+        
+      </ToastContainer>
     </div>
   );
 }
