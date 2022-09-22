ea6f86bd0f7ff7cbc60a66cd25fdc5d62ef9ba58
edoardodominikus <s108006222@m108.nthu.edu.tw>
2022-09-22 17:35:14 +0800

    reverted files

---
+++ src/parts/Testimony.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+import Star from 'elements/Star'
+import Button from 'elements/Button'
+import TestimonyAccent from 'assets/images/testimonial-landingpages-frame.jpg'
+import { Fade } from 'react-reveal'
+
+export default class Testimony extends Component {
+  render() {
+    return (
+      <Fade bottom>
+          <section className='container'>
+          <div className="row align-items-center">
+            <div className="col" style={{marginRight: 60}}>
+              <div className="testimonial-hero" style={{margin: `30px 0 0 30px`}}>
+                <img src={`${process.env.REACT_APP_HOST}/${this.props.data.imageUrl}`} alt="Testimonial" className='position-absolute' style={{zIndex:2}}/>
+                <img src={TestimonyAccent} alt="Testimonial frame" className='position-absolute' style={{margin:`-30px 0 0 -30px`, zIndex:1}}/>
+              </div>
+            </div>
+            <div className="col-6">
+              <h4 style={{marginBottom: 40}}>
+                {this.props.data.name}
+              </h4>
+              <Star value={this.props.data.rate} width={35} height={35} spacing={4}></Star>
+              <h5 className="h2 font-weight-light line-height-2 my-3">
+                {this.props.data.content}
+              </h5>
+              <span className="text-gray-500">
+                {this.props.data.familyName}, {this.props.data.familyOccupation}
+              </span>
+              <div>
+                <Button className='btn px-5' style={{marginTop: 40}} hasShadow isPrimary type="link" href={`/testimonial/${this.props.data._id}`}>
+                  Read Their Story
+                </Button>
+              </div>
+            </div>
+          </div>
+        </section> 
+      </Fade>
+      
+    )
+  }
+}
